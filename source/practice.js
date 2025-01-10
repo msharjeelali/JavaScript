bc172db03d46b8f2f0b0a4e8fbb6f8cbcfbe8b4b
msharjeelali <msharjeelali@yahoo.com>
2025-01-10 14:33:26 +0000

    Two practice questions from youtube tutorial.

---
+++ source/practice.js
@@ -0,0 +1,24 @@
+/* Store inforamtion about a speciific product on e-commerce store. */
+
+const product = {
+    name: "Ball Pen",
+    price: 270,
+    rating: 4,
+    discount: 5,
+    isDeal: true
+};
+
+console.log(product);
+
+/* Store information about soical accoount profile. */
+
+const profile = {
+    fullName: "Muahammad Sharjeel Ali",
+    posts: 200,
+    followers: 1000,
+    followings: 100,
+    username: "msharjeelali",
+    bio: "Front-End Developer"
+};
+
+console.log(profile);
