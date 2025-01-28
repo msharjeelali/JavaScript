9c88a0b93d71ca0ecd06ef623c580356c22d7db5
msharjeelali <msharjeelali@yahoo.com>
2025-01-28 12:02:22 +0000

    For each method for array.

--- source/functions.js
+++ source/functions.js
@@ -1,3 +1,5 @@
+// Simple function definition
+
 function countVowels(inputString){
     let x = 0;
     let lowerString = inputString.toLowerCase();
@@ -7,9 +9,10 @@ function countVowels(inputString){
     }
     return x;
 }
-
 console.log(countVowels("Elephant"));
 console.log(countVowels("abcdefghijk"));
+
+// Arrow functions
 
 const arrowCountVowels = (inputString) => {
     let x = 0;
@@ -20,6 +23,10 @@ const arrowCountVowels = (inputString) => {
     }
     return x;
 }
-
 console.log(arrowCountVowels("Elephant"));
 console.log(arrowCountVowels("abcdefghijk"));
+
+// For each function for arrays
+
+let array = [1,2,3,4,5,6,7,8,9,10];
+array.forEach((val) => {console.log(val*val);});
