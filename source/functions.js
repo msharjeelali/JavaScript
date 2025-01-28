d3c3735d660e4ded8ae4df810fa9fa422a3ddeb0
msharjeelali <msharjeelali@yahoo.com>
2025-01-28 13:12:32 +0000

    Reduce method in arrays.

--- source/functions.js
+++ source/functions.js
@@ -9,8 +9,8 @@ function countVowels(inputString){
     }
     return x;
 }
-console.log(countVowels("Elephant"));
-console.log(countVowels("abcdefghijk"));
+//console.log(countVowels("Elephant"));
+//console.log(countVowels("abcdefghijk"));
 
 // Arrow functions
 
@@ -23,10 +23,37 @@ const arrowCountVowels = (inputString) => {
     }
     return x;
 }
-console.log(arrowCountVowels("Elephant"));
-console.log(arrowCountVowels("abcdefghijk"));
+//console.log(arrowCountVowels("Elephant"));
+//console.log(arrowCountVowels("abcdefghijk"));
 
 // For each function for arrays
 
 let array = [1,2,3,4,5,6,7,8,9,10];
-array.forEach((val) => {console.log(val*val);});
+//array.forEach((val) => {console.log(val*val);});
+
+// Filter method
+
+let marks = [23,45,101,93,45,889,76,56];
+let filteredMarks = marks.filter((val)=>{
+    return val > 90;
+});
+console.log(filteredMarks);
+
+// Reduce method
+
+let n = 10;
+let myArray = [];
+for(let i = 1;i<= n;i++){
+    myArray.push(i);
+}
+let sum = myArray.reduce((prev, curr) => {
+    return prev + curr;
+})
+let product = myArray.reduce((prev,curr) => {
+    return prev*curr;
+})
+
+console.log(myArray);
+console.log(sum);
+console.log(product);
+
