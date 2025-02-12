b4da4cbce2d26665937eb632dc9e0b2465c6be65
Muhammad Sharjeel Ali <msharjeelali@yahoo.com>
2025-02-12 20:43:44 +0500

    Create switch.js

---
+++ source/switch.js
@@ -0,0 +1,20 @@
+let month = 1;
+
+switch (month) {
+    case 1:
+        console.log("Januaury");
+        break;
+    case 2:
+        console.log("Febuaury");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case");
+        break;
+}
